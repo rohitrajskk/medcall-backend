2473d98d418a5496a900670cdf7a57a590a80dde
Rohit Kumar <rohit@rtbrick.com>
2021-03-28 02:31:28 +0530

    Added new endpoints for the following task [1. Add/get doctor 2. Add/get medical shop 3. update/get prescription to an appointment 4. update/get referral to an appointment]

--- app/server/app.py
+++ app/server/app.py
@@ -5,15 +5,17 @@ import server.database as database
 from pydantic import BaseModel
 from enum import Enum
 from fastapi.middleware.cors import CORSMiddleware
-origins = [
-    "*"
-]
 from fastapi.openapi.docs import (
     get_redoc_html,
     get_swagger_ui_html,
     get_swagger_ui_oauth2_redirect_html,
 )
 from fastapi.staticfiles import StaticFiles
+
+origins = [
+    "*"
+]
+
 app = FastAPI(docs_url=None, redoc_url=None)
 app.add_middleware(
     CORSMiddleware,
@@ -23,6 +25,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(
@@ -33,9 +37,11 @@ async def custom_swagger_ui_html():
         swagger_css_url="/static/swagger-ui.css",
     )
 
+
 @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
 async def swagger_ui_redirect():
     return get_swagger_ui_oauth2_redirect_html()
+
 
 @app.get("/redoc", include_in_schema=False)
 async def redoc_html():
@@ -44,6 +50,8 @@ async def redoc_html():
         title=app.title + " - ReDoc",
         redoc_js_url="/static/redoc.standalone.js",
     )
+
+
 class Patient(BaseModel):
     name: str
     mobile_no: int
@@ -52,35 +60,48 @@ class Patient(BaseModel):
     gender: str
     pre_existing_medical_condition: List[str] = None
 
+
 class DoctorType(Enum):
     INHOUSE = 1
     EXTERNAL = 2
 
-class address(BaseModel):
-        name: str
-        mobile_no: int
-        pincode: int
-        landmark : str
-        address_str: str
-        city: str
-        state: str
-        google_map: str
 
-class doctor(BaseModel):
+class Address(BaseModel):
+    name: str
+    mobile_no: int
+    pin_code: int
+    landmark: str
+    address_str: str
+    city: str
+    state: str
+    google_map: str
+
+
+class Doctor(BaseModel):
     mobile_no: int
     alternate_mobile_no: Optional[int]
     doctor_name: str
-    availibility: DoctorType
+    availability: DoctorType
     degree: str
-    specilisation: str
+    specialisation: str
     registration_no: Optional[str]
     consultation_fee: int
-    rating: float
-    doctor_address: address
+    rating: Optional[float]
+    doctor_address: Address
     service_timing_week: Optional[str]
     service_timing_day: Optional[str]
 
-class vital(BaseModel):
+
+class MedicalShop(BaseModel):
+    mobile_no: int
+    alternate_mobile_no: Optional[int]
+    medical_shop_name: str
+    registration_no: Optional[str]
+    rating: Optional[float]
+    medical_shop_address: Address
+
+
+class Vital(BaseModel):
     body_temperature: float
     pulse_rate: int
     respiration_rate: int
@@ -88,124 +109,209 @@ class vital(BaseModel):
     blood_pressure_dia: int
     blood_sugar: int
 
-class medicine(BaseModel):
+
+class Medicine(BaseModel):
     medicine_name: str
     brand_name: str
     medicine_mg: str
     treatment_gap_day: Optional[int]
     treatment_time: List[str]
-    treatment_food_dependecy: Optional[str]
+    treatment_food_dependency: Optional[str]
     treatment_instruction: Optional[str]
     treatment_period: int
 
 
-
-class test(BaseModel):
+class Test(BaseModel):
     test_name: str
     lab_name: Optional[str]
-    lab_address: address
+    lab_address: Address
     test_comment: str
 
-class prescription(BaseModel):
+
+class Prescription(BaseModel):
     diagnosis: Optional[str]
-    medicines: List[medicine]
-    test: Optional[test]
-    referal_doc_id: Optional[doctor]
-    consulting_doc_id: str
+    medicines: List[Medicine]
+    test: Optional[Test]
     follow_up_date: Optional[int]
     instructions: Optional[str]
 
-class appointment(BaseModel):
-    vitals: vital
+
+class Appointment(BaseModel):
+    vitals: Vital
     patient_id: str
+
+
+class ReferralDoctor(BaseModel):
+    referral_doc_id: str
 
 
 @app.get("/", tags=["Root"])
 async def read_root():
     return {"message": "Welcome to this fantastic app!"}
 
-@app.get("/patient/{id}", tags=["Root"])
-async def get_patients(id):
-    #return {"message": "Return patients"}
-    patients = await database.get_patient(patient_id=id)
-    print(patients)
+
+@app.get("/patient/{patient_id}", tags=["Root"])
+async def get_patients(patient_id):
+    # return {"message": "Return patients"}
+    patients = await database.get_patient(patient_id=patient_id)
+    # print(patients)
     if patients:
         return patients
     else:
         return {"message": "No patients found in the database"}
+
 
 @app.get("/patient", tags=["Root"])
 async def get_patients(mobile_no: Optional[int] = None):
-    #return {"message": "Return patients"}
+    # return {"message": "Return patients"}
     patients = await database.get_patient(mobile_no=mobile_no)
-    print(patients)
+    # print(patients)
     if patients:
         return patients
     else:
         return {"message": "No patients found in the database"}
 
+
 @app.post("/patient", tags=["Root"], )
 async def add_patients(patient: Patient):
-    new_patient  = await database.add_patient(patient.dict())
-    return { "patient_id": str(new_patient.inserted_id), "message": "Sucessfully added patient"}
+    new_patient = await database.add_patient(patient.dict())
+    return {"patient_id": str(new_patient.inserted_id), "message": "Successfully added patient"}
 
-@app.get("/patient/{id}/appointment/{appointment_id}", tags=["Root"])
-async def get_appointments(id, appointment_id):
-    #return {"message": "Return patients"}
-    appointment = await database.get_appointment(patient_id=id, appointment_id=appointment_id)
+
+@app.get("/doctor/{doctor_id}", tags=["Root"])
+async def get_doctor(doctor_id):
+    doctor = await database.get_doctor(doctor_id=doctor_id)
+    if doctor:
+        return doctor
+    else:
+        return {"message": "No doctor with ID; {} found in the database".format(doctor_id)}
+
+
+@app.get("/doctor", tags=["Root"])
+async def get_doctor(mobile_no: Optional[int] = None):
+    doctors = await database.get_doctor(mobile_no=mobile_no)
+    if doctors:
+        return doctors
+    else:
+        return {"message": "No doctors found in the database"}
+
+
+@app.post("/doctor", tags=["Root"], )
+async def add_doctor(doctor: Doctor):
+    new_doctor = await database.add_doctor(doctor.dict())
+    return {"doctor_id": str(doctor.inserted_id), "message": "Successfully added doctor"}
+
+
+@app.get("/medical-shop/{shop_id}", tags=["Root"])
+async def get_medical_shop(shop_id):
+    medical_shop = await database.get_medical_shop(shop_id=shop_id)
+    if medical_shop:
+        return medical_shop
+    else:
+        return {"message": "No medical shop with ID; {} found in the database".format(shop_id)}
+
+
+@app.get("/medical-shop", tags=["Root"])
+async def get_medical_shop(mobile_no: Optional[int] = None):
+    medical_shop = await database.get_medical_shop(mobile_no=mobile_no)
+    if medical_shop:
+        return medical_shop
+    else:
+        return {"message": "No medical shop found in the database"}
+
+
+@app.post("/medical-shop", tags=["Root"], )
+async def add_medical_shop(medical_shop: MedicalShop):
+    new_medical_shop = await database.add_medical_shop(medical_shop.dict())
+    return {"doctor_id": str(new_medical_shop.inserted_id), "message": "Successfully added medicla shop"}
+
+
+@app.get("/patient/{patient_id}/appointment/{appointment_id}", tags=["Root"])
+async def get_appointments(patient_id, appointment_id):
+    # return {"message": "Return patients"}
+    appointment = await database.get_appointment(patient_id=patient_id, appointment_id=appointment_id)
     if appointment:
         return appointment
     else:
         return {"message": "No appointment found in the database"}
 
-@app.get("/patient/{id}/appointment", tags=["Root"])
-async def get_appointments(id):
-    #return {"message": "Return patients"}
-    appointment = await database.get_appointment(patient_id=id)
+
+@app.get("/patient/{patient_id}/appointment", tags=["Root"])
+async def get_appointments(patient_id):
+    # return {"message": "Return patients"}
+    appointment = await database.get_appointment(patient_id=patient_id)
     if appointment:
         return appointment
     else:
         return {"message": "No appointment found in the database"}
 
-@app.post("/patient/{id}/appointment", tags=["Root"])
-async def create_appointment(id, appointment: vital):
-    new_appointment = await database.create_appointment(patient_id=id, vital=appointment.dict())
+
+@app.post("/patient/{patient_id}/appointment", tags=["Root"])
+async def create_appointment(patient_id, appointment: Vital):
+    new_appointment = await database.create_appointment(patient_id=patient_id, vital=appointment.dict())
     if new_appointment is not None:
-        print(new_appointment)
-        return { "appointment_id": str(new_appointment.inserted_id), "message": "Sucessfully added patient"}
+        # print(new_appointment)
+        return {"appointment_id": str(new_appointment.inserted_id), "message": "Successfully added patient"}
     else:
-        return { "message": "Parent ID: {} not found in database".format(id)}
+        return {"message": "Parent ID: {} not found in database".format(patient_id)}
 
-@app.post("/patient/{id}/appointment/{appointment_id}/prescription", tags=["Root"])
-async def add_prescription(id, appointment_id, prescription: prescription):
-    #return {"message": "Return patients"}
-    appointment = await database.add_prescription(patient_id=id, appointment_id=appointment_id, prescription=prescription)
-    if appointment:
-        return appointment
-    else:
-        return {"message": "No appointment found in the database"}
 
-@app.get("/patient/{id}/appointment/{appointment_id}/prescription", tags=["Root"])
-async def get_prescription(id, appointment_id):
-    #return {"message": "Return patients"}
-    appointment = await database.get_appointment(patient_id=id, appointment_id=appointment_id)
-    if appointment:
-        return appointment
+@app.post("/patient/{patient_id}/appointment/{appointment_id}/prescription", tags=["Root"])
+async def add_prescription(patient_id, appointment_id, prescription: Prescription):
+    # return {"message": "Return patients"}
+    new_prescription = await database.add_appointment_referral(referral_doctor=prescription.dict(),
+                                                               patient_id=patient_id, appointment_id=appointment_id)
+    if new_prescription:
+        return {"_id": appointment_id, "message": "Successfully updated prescription"}
     else:
-        return {"message": "No appointment found in the database"}
+        return {"message": "No appointment found with ID: {} in the database".format(appointment_id)}
+
+
+@app.get("/patient/{patient_id}/appointment/{appointment_id}/prescription", tags=["Root"])
+async def get_prescription(patient_id, appointment_id):
+    appointment = await database.get_appointment(patient_id=patient_id, appointment_id=appointment_id)
+    if appointment is None:
+        return {"message": "No appointment found with ID: {} in the database".format(appointment_id)}
+    elif appointment.get("prescription"):
+        return appointment.get("prescription")
+    else:
+        return {"message": "No prescription found in the database"}
+
+
+@app.post("/patient/{patient_id}/appointment/{appointment_id}/referral", tags=["Root"])
+async def add_referral(patient_id, appointment_id, referral: ReferralDoctor):
+    new_referral = await database.add_appointment_referral(patient_id=patient_id, appointment_id=appointment_id,
+                                                           referral_doctor=referral.dict())
+    if new_referral:
+        return {"_id": appointment_id, "message": "Successfully updated referral"}
+    else:
+        return {"message": "No appointment found with ID: {} in the database".format(appointment_id)}
+
+
+@app.get("/patient/{patient_id}/appointment/{appointment_id}/referral", tags=["Root"])
+async def get_referral(patient_id, appointment_id):
+    appointment = await database.get_appointment(patient_id=patient_id, appointment_id=appointment_id)
+    if appointment is None:
+        return {"message": "No appointment found with ID: {} in the database".format(appointment_id)}
+    elif appointment.get("referral"):
+        return appointment.get("referral")
+    else:
+        return {"message": "No referral found in the database"}
+
 
 @app.get("/active/appointment", tags=["Root"])
 async def get_appointments():
-    #return {"message": "Return patients"}
+    # return {"message": "Return patients"}
     appointment = await database.active_appointment()
     if appointment:
         return appointment
     else:
         return {"message": "No appointment found in the database"}
 
+
 @app.get("/inactive/appointment", tags=["Root"])
 async def get_appointments():
-    #return {"message": "Return patients"}
+    # return {"message": "Return patients"}
     appointment = await database.inactive_appointment()
     if appointment:
         return appointment
--- app/server/database.py
+++ app/server/database.py
@@ -5,16 +5,25 @@ from datetime import datetime
 from enum import Enum
 import server.daily_co_meeting as meeting
 
+
 class DocType(Enum):
     FAMILY_USER = 1
     PATIENT_USER = 2
     APPOINTMENT = 3
+
+
+class UserRole(Enum):
+    DOCTOR = 0
+    MEDICALSHOP = 1
+    PATIENT = 2
+
 
 class AppointmentStatus(Enum):
     CREATED = 1
     IN_QUEUE = 2
     VIDEO_CALL = 3
     COMPLETED = 4
+
 
 MONGO_DETAILS = "mongodb://localhost:27017"
 
@@ -25,7 +34,16 @@ database = client.patients
 patient_collection = database.get_collection("patient_collection7")
 patient_collection.create_index("parent")
 patient_collection.create_index("doc_type")
-patient_collection.create_index([("mobile_no", pymongo.DESCENDING), ("name", pymongo.DESCENDING), ("doc_type", pymongo.DESCENDING), ("time", pymongo.DESCENDING)], unique=True)
+patient_collection.create_index(
+    [("mobile_no", pymongo.DESCENDING), ("name", pymongo.DESCENDING), ("doc_type", pymongo.DESCENDING),
+     ("time", pymongo.DESCENDING)], unique=True)
+
+doctor_collection = database.get_collection("doctor_collection")
+doctor_collection.create_index([("mobile_no", pymongo.DESCENDING)], unique=True)
+
+medical_shop_collection = database.get_collection("medical_shop_collection")
+medical_shop_collection.create_index([("mobile_no", pymongo.DESCENDING)], unique=True)
+
 
 async def get_patient(patient_id=None, mobile_no=None):
     if patient_id:
@@ -45,23 +63,73 @@ async def get_patient(patient_id=None, mobile_no=None):
 
     return patients
 
+
 # Add a new patient into to the database
 async def add_patient(patient_data: dict) -> dict:
     parent_user = await patient_collection.find_one({"mobile_no": patient_data["mobile_no"], "parent": None})
-    #print(parent_user)
+    # print(parent_user)
     if not parent_user:
-        patient = await patient_collection.insert_one({"mobile_no": patient_data["mobile_no"], "doc_type": DocType.FAMILY_USER.value})
-        #print(patient)
+        patient = await patient_collection.insert_one(
+            {"mobile_no": patient_data["mobile_no"], "doc_type": DocType.FAMILY_USER.value, "user_role": UserRole.PATIENT.value})
+        # print(patient)
         patient_data["parent"] = str(patient.inserted_id)
         patient_data["doc_type"] = DocType.PATIENT_USER.value
+        patient_data["user_role"] = UserRole.PATIENT.value
         new_patient = await patient_collection.insert_one(patient_data)
     else:
         patient_data["parent"] = str(parent_user["_id"])
         patient_data["doc_type"] = DocType.PATIENT_USER.value
+        patient_data["user_role"] = UserRole.PATIENT.value
         new_patient = await patient_collection.insert_one(patient_data)
     return new_patient
 
 
+async def get_doctor(doctor_id=None, mobile_no=None):
+    if doctor_id:
+        doctor = await doctor_collection.find_one({"_id": ObjectId(doctor_id)})
+        if doctor:
+            return patient_helper(doctor)
+        return None
+    elif mobile_no:
+        doctor = await patient_collection.find_one({"mobile_no": mobile_no})
+        if doctor:
+            return patient_helper(doctor)
+        return None
+    else:
+        doctors = []
+        async for doctor in doctor_collection.find():
+            doctors.append(patient_helper(doctor))
+        return doctors
+
+
+async def add_doctor(doctor_data: dict) -> dict:
+    doctor_data["user_role"] = UserRole.DOCTOR.value
+    new_doctor = await doctor_collection.insert_one(doctor_data)
+    return new_doctor
+
+
+async def get_medical_shop(shop_id=None, mobile_no=None):
+    if shop_id:
+        shop = await medical_shop_collection.find_one({"_id": ObjectId(shop_id)})
+        if shop:
+            return patient_helper(shop)
+        return None
+    elif mobile_no:
+        shop = await medical_shop_collection.find_one({"mobile_no": mobile_no})
+        if shop:
+            return patient_helper(shop)
+        return None
+    else:
+        shops = []
+        async for shop in medical_shop_collection.find():
+            shops.append(patient_helper(shop))
+        return shops
+
+
+async def add_medical_shop(shop_data: dict) -> dict:
+    shop_data["user_role"] = UserRole.MEDICALSHOP.value
+    new_shop = await medical_shop_collection.insert_one(shop_data)
+    return new_shop
 
 
 async def create_appointment(patient_id, vital: dict):
@@ -76,18 +144,18 @@ async def create_appointment(patient_id, vital: dict):
     appointment["doc_type"] = DocType.APPOINTMENT.value
     appointment["status"] = AppointmentStatus.CREATED.value
     appointment["is_active"] = True
-    daily_co_meeting= meeting.getmeetings(patient_id=patient_id)
+    daily_co_meeting = meeting.getmeetings(patient_id=patient_id)
     appointment["meeting"] = {
         "doctor_meeting_url": daily_co_meeting["url"],
         "patient_meeting_url": daily_co_meeting["url"],
         "meeting_id": daily_co_meeting["id"]
     }
-    appointment["prescription"] = False
-    appointment["referral"] = False
-    appointment["old_prescription"] = False
+    appointment["prescription"] = None
+    appointment["referral"] = None
     new_appointment = await patient_collection.insert_one(appointment)
-    print(new_appointment)
+    # print(new_appointment)
     return new_appointment
+
 
 async def get_appointment(patient_id, appointment_id=None):
     if appointment_id is None:
@@ -98,6 +166,41 @@ async def get_appointment(patient_id, appointment_id=None):
     else:
         appointment = await patient_collection.find_one({"_id": ObjectId(appointment_id)})
         return patient_helper(appointment)
+
+
+async def add_appointment_prescription(prescription: dict, patient_id=None, appointment_id=None):
+    """
+    :type prescription: object
+    :param appointment_id:
+    :param patient_id:
+    """
+    if appointment_id is None:
+        return None
+    else:
+        result = await patient_collection.update_one({"_id": ObjectId(appointment_id)},
+                                                     {"$set": {"prescription": prescription}})
+        if result.modified_count == 1:
+            return appointment_id
+        else:
+            return None
+
+
+async def add_appointment_referral(referral_doctor: dict, patient_id=None, appointment_id=None):
+    """
+    :type referral_doctor: object
+    :param appointment_id:
+    :param patient_id:
+    """
+    if appointment_id is None:
+        return None
+    else:
+        result = await patient_collection.update_one({"_id": ObjectId(appointment_id)},
+                                                     {"$set": {"referral": referral_doctor.get("referral_doc_id")}})
+        if result.modified_count == 1:
+            return appointment_id
+        else:
+            return None
+
 
 async def active_appointment():
     appointments = []
@@ -114,6 +217,8 @@ async def inactive_appointment():
 
 
 def patient_helper(patient) -> dict:
-    #print(patient)
+    # print(patient)
     patient["_id"] = str(patient["_id"])
+    if patient.get("user_role"):
+        patient["user_role"] = UserRole(patient.get("user_role")).name
     return patient
